1d8b1d04fa007b5af784ad24abbed19282a733cc
krishnaCG-1997 <krishna.a.agarwal@capgemini.com>
2021-12-20 19:25:42 +0530

    triggering test-3

--- src/app/core/user/services/user.service.ts
+++ src/app/core/user/services/user.service.ts
@@ -9,8 +9,8 @@ import { ChangePasswordDTO } from '../../auth/model/ChangePasswordDTO';
 import { IUserTeamService } from './user-team.service.interface';
 import { IUserService } from './user.service.interface';
 import { IUserSessionDetailsService } from './user-session-details.service.interface';
-// import { IEmailService } from '../../../email/services/email.service.interface';
-// import { SendEmailDTO } from '../../../email/model/dto/SendEmail.dto';
+import { IEmailService } from '../../../email/services/email.service.interface';
+import { SendEmailDTO } from '../../../email/model/dto/SendEmail.dto';
 
 var generator = require('generate-password');
 
@@ -20,7 +20,8 @@ export class UserService extends TypeOrmCrudService<User> implements IUserServic
     @InjectRepository(User) private readonly userRepository: Repository<User>,
     //@InjectRepository(UserSessionDetails) private readonly userInfoRepository: Repository<UserSessionDetails>,
     @Inject('IUserTeamService') private readonly userTeamService: IUserTeamService,
-    @Inject('IUserSessionDetailsService') private readonly userSessionDetailsService: IUserSessionDetailsService, //private readonly userTeamService: UserTeamService, // @Inject('IEmailService') private readonly emailService: IEmailService,
+    @Inject('IUserSessionDetailsService') private readonly userSessionDetailsService: IUserSessionDetailsService, //private readonly userTeamService: UserTeamService,
+    @Inject('IEmailService') private readonly emailService: IEmailService,
   ) {
     super(userRepository);
   }
@@ -61,13 +62,12 @@ export class UserService extends TypeOrmCrudService<User> implements IUserServic
     console.log(result.id);
     //save the user session object into the DB here
     if (result) {
-      // let sendEmailDTO: SendEmailDTO = {} as SendEmailDTO;
-      // sendEmailDTO.username = result.username;
-      // sendEmailDTO.toEmail = result.email;
-      // sendEmailDTO.defaultPassword = password;
-      // this.emailService.sendTeamplateEmail(sendEmailDTO);
-      // this.userSessionDetailsService.registerUserIntoUserSession(result.id);
-      console.log('Successssss');
+      let sendEmailDTO: SendEmailDTO = {} as SendEmailDTO;
+      sendEmailDTO.username = result.username;
+      sendEmailDTO.toEmail = result.email;
+      sendEmailDTO.defaultPassword = password;
+      this.emailService.sendTeamplateEmail(sendEmailDTO);
+      this.userSessionDetailsService.registerUserIntoUserSession(result.id);
       return this.userTeamService.addUserToTeam(result, userDTO);
     }
   }
