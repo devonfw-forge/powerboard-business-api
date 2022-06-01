5f22dfae56bb5c372d74b89c1ad57e9f0c236e0a
LANJEWAR <sakshi-sharad.lanjewar@capgemini.com>
2022-06-01 12:07:02 +0530

    trigger api pipeline 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,13 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CoreModule } from './core/core.module';
 import { DashboardModule } from './dashboard/dashboard.module';
-import { EmailModule } from './email/email.module';
+/* import { EmailModule } from './email/email.module'; */
 import { MultimediaModule } from './multimedia/multimedia.module';
 import { TeamLinksModule } from './team-links/team-links.module';
 import { VisibilityModule } from './visibility/visibility.module';
 
 @Module({
-  imports: [CoreModule, DashboardModule, TeamLinksModule, MultimediaModule, VisibilityModule, EmailModule],
+  imports: [CoreModule, DashboardModule, TeamLinksModule, MultimediaModule, VisibilityModule],
   controllers: [AppController],
   providers: [AppService],
 })
